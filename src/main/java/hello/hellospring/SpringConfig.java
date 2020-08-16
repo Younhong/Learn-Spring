30243f762505944db8b6a0ab76c7d79a13700b9d
Younhong <younhong@kakao.com>
2020-08-16 22:47:14 +0900

    AOP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -3,6 +3,7 @@ package hello.hellospring;
 //import hello.hellospring.repository.JdbcMemberRepository;
 //import hello.hellospring.repository.JpaMemberRepository;
 //import hello.hellospring.repository.JdbcTemplateMemberRepository;
+//import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
 //import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -35,6 +36,11 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository() {
