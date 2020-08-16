df29380cb4550b81f41a836a7f5c93b721c43121
Younhong <younhong@kakao.com>
2020-08-16 19:05:26 +0900

    jpa data

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -2,8 +2,8 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assert;
-import org.assertj.core.api.Assertions;
+//import org.assertj.core.api.Assert;
+//import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
